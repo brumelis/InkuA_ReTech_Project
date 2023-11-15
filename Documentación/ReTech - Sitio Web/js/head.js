5c286e12ce74b1f39f3211b74b0dad58fb23cdee
diegodistefano <diego.distefano.1989@gmail.com>
2023-11-15 16:07:21 -0300

    Organizando el CSS

--- Documentación/ReTech - Sitio Web/js/head.js
+++ Documentación/ReTech - Sitio Web/js/head.js
@@ -4,7 +4,7 @@ head.innerHTML = `
 <head class="head">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles2.css">
+    <link rel="stylesheet" href="./styles.css">
     <link rel="shortcut icon" href="./ReTech - Sitio Web/images-Sprint1/favicon.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
